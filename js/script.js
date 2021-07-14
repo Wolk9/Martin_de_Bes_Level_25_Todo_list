388cd30eb014a135b625262c518fdb69e47b7237
Wolk9 <66083478+Wolk9@users.noreply.github.com>
2021-07-14 23:30:34 +0200

    Verbeteren van de code. Fetch POST werkt nu niet.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const data = { description: "buy oatmeal", done: false };
+const data = 0;
 const baseUrl = "http://localhost:3000/";
 const id2 = "cd61b1d1-560d-4102-8cc7-85c614f8753c";
 const id = "";
@@ -26,35 +26,53 @@ const inputForm = () => {
   const createInputField = document.createElement("input");
   createInputField.setAttribute("type", "text");
   createInputField.setAttribute("name", "newTask");
-  createInputField.setAttribute("value", "new task");
+  createInputField.setAttribute("placeholder", "Type a task...");
   createInputForm.appendChild(createInputField);
+  // document.querySelector("input").focus();
   const submitButton = document.createElement("button");
   submitButton.setAttribute("id", "submit");
   createInputForm.appendChild(submitButton);
-  submitButton.innerText = "create";
-  submitButton.addEventListener("mousedown", function () {
+  submitButton.innerText = "Create Task";
+  submitButton.addEventListener("click", function () {
+    const input = document.querySelector("input").value;
     createData(input);
+  });
+  createInputField.addEventListener("keydown", function (event) {
+    if (event.key != "Enter") {
+      console.log(event.key + " is geen Enter");
+    } else {
+      const input = document.querySelector("input").value;
+      createData(input);
+    }
   });
 };
 
-// hier ben ik gebleven!
-
 async function createData(input) {
-  const content = {
-    "'description'": input,
-    "'done'": false
-  }.json;
-  const response = await fetch(baseUrl + id, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: content
+  console.log(input);
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    description: input,
+    done: false
   });
-  const json = await response.json();
-  console.log(response);
-  console.log(json);
-  return displayData(json);
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow"
+  };
+
+  await fetch(baseUrl, requestOptions)
+    .then((response) => {
+      response.text();
+    })
+    .then((result) => {
+      console.log(result);
+      // catchData();
+    })
+    .catch((error) => console.log("error", error));
 }
 
 async function catchData() {
@@ -85,87 +103,51 @@ async function deleteData(id) {
 
 const displayData = (data) => {
   console.log(data, data.length);
-  const listPlace = document.getElementById("todo");
 
-  if (document.getElementById("todo").children.length >= 1) {
-    // console.log(document.getElementById("todo").children.length);
-    const div = document.getElementById("todo");
-    div.children[0].remove();
-  }
-  console.log(document.getElementById("todo").children.length);
-  const div = document.createElement("div");
-  const table = document.createElement("table");
-  div.setAttribute("id", "tablediv");
-  table.setAttribute("id", "table");
-  listPlace.appendChild(div);
-  div.appendChild(table);
+  const displayRow = () => {
+    data.forEach((item) => {
+      const tableDiv = document.getElementById("tablediv");
+      const table = document.getElementById("table");
+      const tableRow = document.createElement("tr");
+      const idValue = item._id;
+      const createDoneCell = document.createElement("td");
+      const createDescriptionCell = document.createElement("td");
+      const createDeleteCell = document.createElement("td");
+      const doneCell = tableRow.appendChild(createDoneCell);
+      doneCell.setAttribute("width", "80px");
+      const descriptionCell = tableRow.appendChild(createDescriptionCell);
+      descriptionCell.setAttribute("width", "400px");
+      const deleteCell = tableRow.appendChild(createDeleteCell);
 
-  data.forEach((item) => {
-    const div = document.getElementById("todo");
-    const table = document.getElementById("tablediv");
-    const tableRow = document.createElement("tr");
-    const idValue = item._id;
-    const createDoneCell = document.createElement("td");
-    const createDescriptionCell = document.createElement("td");
-    const createDeleteCell = document.createElement("td");
-    const doneCell = tableRow.appendChild(createDoneCell);
-    doneCell.setAttribute("width", "80px");
-    const descriptionCell = tableRow.appendChild(createDescriptionCell);
-    descriptionCell.setAttribute("width", "400px");
-    const deleteCell = tableRow.appendChild(createDeleteCell);
-
-    const createTrashCanButton = document.createElement("button");
-    createTrashCanButton.classList.add("btn");
-    createTrashCanButton.addEventListener("mousedown", function () {
-      deleteData(idValue);
+      const createTrashCanButton = document.createElement("button");
+      createTrashCanButton.classList.add("btn");
+      createTrashCanButton.addEventListener("mousedown", function () {
+        deleteData(idValue);
+      });
+      const createTrashCan = document.createElement("i");
+      createTrashCan.classList.add("fa", "fa-trash");
+      deleteCell.appendChild(createTrashCanButton);
+      createTrashCanButton.appendChild(createTrashCan);
+      doneCell.innerText = item.done;
+      descriptionCell.innerText = item.description;
+      table.appendChild(tableRow);
+      tableDiv.appendChild(table);
     });
-    const createTrashCan = document.createElement("i");
-    createTrashCan.classList.add("fa", "fa-trash");
-    deleteCell.appendChild(createTrashCanButton);
-    createTrashCanButton.appendChild(createTrashCan);
-    doneCell.innerText = item.done;
-    descriptionCell.innerText = item.description;
-    table.appendChild(tableRow);
-  });
+  };
+
+  if (data.length <= 1) {
+    const listPlace = document.getElementById("todo");
+    const createDiv = document.createElement("div");
+    const createTable = document.createElement("table");
+    createDiv.setAttribute("id", "tablediv");
+    createTable.setAttribute("id", "table");
+    listPlace.appendChild(createDiv);
+    createDiv.appendChild(createTable);
+
+    displayRow();
+  } else if (data.length > 1) {
+    displayRow();
+  }
 };
 
 inputForm();
-
-// fetch(baseUrl, {
-//   method: "POST",
-//   body: JSON.stringify(data),
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// });
-
-// console.log(data);
-
-// async function getList() {
-// const dataList =  fetch.get() =>
-
-// }
-// console.log(dataList);
-
-// // Example POST method implementation:
-// async function postData(url = "", data = {}) {
-//   // Default options are marked with *
-//   const response = await fetch(url, {
-//     method: "POST", // *GET, POST, PUT, DELETE, etc.
-//     mode: "cors", // no-cors, *cors, same-origin
-//     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-//     credentials: "same-origin", // include, *same-origin, omit
-//     headers: {
-//       "Content-Type": "application/json"
-//       // 'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     redirect: "follow", // manual, *follow, error
-//     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-//     body: JSON.stringify(data) // body data type must match "Content-Type" header
-//   });
-//   return response.json(); // parses JSON response into native JavaScript objects
-// }
-
-// postData("https://example.com/answer", { answer: 42 }).then((data) => {
-//   console.log(data); // JSON data parsed by `data.json()` call
-// });
